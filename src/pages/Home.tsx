2ce9caa1206a661523ee350f34382408f0ad0933
Edward <edwardwu132@berkeley.edu>
2024-08-27 14:26:18 -0700

    fix maybe

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function Home() {
+    return <div>Welcome to the Home Page</div>;
+  }
+  
+  export default Home;
