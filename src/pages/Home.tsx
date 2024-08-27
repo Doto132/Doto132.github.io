425eef0fd448a38ec45c38fb640fdf3fa1fda9ff
Edward <edwardwu132@berkeley.edu>
2024-08-27 14:28:02 -0700

    fix maybe 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function Home() {
