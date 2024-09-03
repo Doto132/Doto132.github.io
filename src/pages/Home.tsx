0fa8eb1b9259ee78e7b91c49ed7226392b74b740
Edward Wu <edwardwu132@berkeley.edu>
2024-09-02 19:59:41 -0700

    to pc from laptop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button } from 'react-bootstrap';
+
 
 const Home: React.FC = () => {
   return (
     <div>
+    <Navbar className='navbar-custom' bg="dark" variant="dark">
+      <Navbar.Brand className="ml-auto navbar-brand-custom" href="/">MyWebsite</Navbar.Brand>
+      <Nav className="navbar-nav-custom">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/About">About</Nav.Link>
+      </Nav>
+    </Navbar>
+    <div className="container mt-3">
       <h1>Welcome to the Home Page</h1>
       <p>tweaked with the lappy.</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <Button variant="primary">Click Me</Button>
     </div>
+  </div>
   );
 };
 
